81c8e3a5b0086f7c4f07f9f0eed7b7b0c5fb992a
Camila Modena <itscamilla@hotmail.com>
2020-11-24 15:06:34 +0000

    Completed Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,45 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    title = params[:task][:title]
+    details = params[:task][:details]
+    task = Task.new(title: title, details: details)
+    task.save
+
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
